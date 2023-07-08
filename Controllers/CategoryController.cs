f354d688993e5e7f8815e6acad100351ed22e6de
TrungHieu0102 <hatrunghieu121@gmail.com>
2023-07-09 03:37:02 +0700

    First commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlTypes;
+using WebBanSach.Data;
+using WebBanSach.Models;
+
+namespace WebBanSach.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBanSach.Models
+{
+    public class Category
+    {
+        [Key]// Khoa chinh
+        public int Id { get; set; }
+        [Required]// Not null
+        public string Name { get; set; }    
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+    }
+}
